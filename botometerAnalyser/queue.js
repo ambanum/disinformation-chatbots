f8c25638e13b9360711a59a60e6b2998939c4545
Nicolas Dupont <npg.dupont@gmail.com>
2019-04-08 16:15:23 +0200

    Add queue manager to handle job concurrency

---
+++ botometerAnalyser/queue.js
@@ -0,0 +1,60 @@
+const config = require('config');
+const request = require('request-promise');
+const Bull = require('bull');
+const botometerAnalyser = require('../botometerAnalyser/index');
+
+const queue = new Bull('Botometer analysis', {
+    limiter: {
+        max: 1,
+        duration: 1000,
+        bounceBack: true,
+    }
+});
+
+queue.process(async (job) => {
+    console.log(`Botometer analyser: Running job for search "${job.data.search}" requested by "${job.data.requesterUsername}"`);
+    return botometerAnalyser(job.data.search);
+});
+
+queue.on('completed', (job, result) => {
+    console.log(`Botometer analyser: Job completed for search "${job.data.search}" requested by "${job.data.requesterUsername}"`);
+    try {
+        request({
+            url: job.data.responseUrl,
+            method: 'POST',
+            json: {
+                text: `@${job.data.requesterUsername} Done!`,
+                response_type: "in_channel",
+                username: job.data.botUsername,
+                icon_url: job.data.botIconUrl,
+                "attachments": [
+                    {
+                        "title": "During the last week",
+                        "fields": [
+                            {
+                                "short": false,
+                                "title": `On the latest ${result.shares.total} shares of "${job.data.search}":`,
+                                "value": `**${result.shares.percentageBot}%** have a high probability to be made by bots\n**${result.shares.percentageHuman}%** have a high probability to be made by humans\nFor the **${result.shares.percentageUnknown}%** others it's difficult to say
+                    `
+                            },
+                            {
+                                "short": false,
+                                "title": `On the ${result.users.total} users that have shared "${job.data.search}" lately:`,
+                                "value": `**${result.users.percentageBot}%** have a high probability to be made by bots\n**${result.users.percentageHuman}%** have a high probability to be made by humans\nFor the **${result.users.percentageUnknown}%** others it's difficult to say`
+                            },
+                        ],
+                    },
+                    {
+                        "title": "Here is the distribution",
+                        "title_link": `${config.get('hooks.domain')}/images/botometerAnalyser/${result.imageUrl}.png`,
+                        "image_url": `${config.get('hooks.domain')}/images/botometerAnalyser/${result.imageUrl}.png`
+                    }
+                ]
+            },
+        })
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+module.exports = queue;
--- botometerAnalyser/routes.js
+++ botometerAnalyser/routes.js
@@ -1,12 +1,15 @@
 const config = require('config');
 const express = require('express');
 const router = express.Router();
-const request = require('request');
+const queue = require('./queue');
 
-const botometerAnalyser = require('../botometerAnalyser/index');
+const botIconUrl = config.get('hooks.botometerAnalyser.mattermost.icon');
+const botUsername = config.get('hooks.botometerAnalyser.mattermost.username');
+const mattermostToken = config.get('hooks.botometerAnalyser.mattermost.token');
 
-router.get('/', function (req, res, next) {
-    if (req.query.token !== config.get('hooks.botometerAnalyser.mattermost.token')) {
+router.get('/', async function (req, res, next) {
+    const givenToken = req.query.token;
+    if (givenToken !== mattermostToken) {
         return res.status(401).json({ error: 'Invalid token' });
     }
 
@@ -14,55 +17,26 @@ router.get('/', function (req, res, next) {
 
     if (!search) {
         return res.json({
-            icon_url: config.get('hooks.botometerAnalyser.mattermost.icon'),
-            username: config.get('hooks.botometerAnalyser.mattermost.username'),
+            icon_url: botIconUrl,
+            username: botUsername,
             text: `Hey! I can help you by analyzing the latest shares on a specific topic but I need a query.\nSo, give me a keyword, an URL or some text. For example: \n\`/botometer disinformation\``
         });
     }
 
     res.json({
         response_type: "in_channel",
-        icon_url: config.get('hooks.botometerAnalyser.mattermost.icon'),
-        username: config.get('hooks.botometerAnalyser.mattermost.username'),
+        icon_url: botIconUrl,
+        username: botUsername,
         text: `Roger! I'm analysing the probability that the accounts (100 max) that have tweeted "${search}" in the past week are robots. This should take 40 minutes max.`
     });
 
-    botometerAnalyser(search)
-        .then((result) => {
-            request({
-                url: req.query.response_url,
-                method: "POST",
-                json: {
-                    text: `@${req.query.user_name} Done!`,
-                    response_type: "in_channel",
-                    username: config.get('hooks.botometerAnalyser.mattermost.username'),
-                    icon_url: config.get('hooks.botometerAnalyser.mattermost.icon'),
-                    "attachments": [
-                        {
-                            "title": "During the last week",
-                            "fields": [
-                                {
-                                    "short": false,
-                                    "title": `On the latest ${result.shares.total} shares of "${search}":`,
-                                    "value": `**${result.shares.percentageBot}%** have a high probability to be made by bots\n**${result.shares.percentageHuman}%** have a high probability to be made by humans\nFor the **${result.shares.percentageUnknown}%** others it's difficult to say
-                  `
-                                },
-                                {
-                                    "short": false,
-                                    "title": `On the ${result.users.total} users that have shared "${search}" lately:`,
-                                    "value": `**${result.users.percentageBot}%** have a high probability to be made by bots\n**${result.users.percentageHuman}%** have a high probability to be made by humans\nFor the **${result.users.percentageUnknown}%** others it's difficult to say`
-                                },
-                            ],
-                        },
-                        {
-                            "title": "Here is the distribution",
-                            "title_link": `${config.get('hooks.domain')}/images/botometerAnalyser/${result.imageUrl}.png`,
-                            "image_url": `${config.get('hooks.domain')}/images/botometerAnalyser/${result.imageUrl}.png`
-                        }
-                    ]
-                },
-            });
-        });
+    await queue.add({
+        search,
+        responseUrl: req.query.response_url,
+        requesterUsername: req.query.user_name,
+        botIconUrl,
+        botUsername,
+    });
 });
 
 module.exports = router;
